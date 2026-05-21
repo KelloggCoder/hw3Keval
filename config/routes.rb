7f7699aaeb93b34c1497b7b1402a3897045a2d65
KelloggCoder <keval.sheth@kellogg.northwestern.edu>
2026-05-21 03:05:36 +0000

    removed sages route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources sages
+
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
   get("/places", { :controller => "places", :action => "index" })
